df252095dcd3d3f9ba75be93d0ff7acebe207a9b
Jalla Lavanya <lavanya123jalla@gmail.com>
2026-03-23 17:41:53 +0530

    first commit

---
+++ src/main/java/com/example/restdemo/security/DemoSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.restdemo.security;
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/api/employees/**").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+        );
+
+        // Enable HTTP Basic auth
+        http.httpBasic(basic -> {});
+
+        // Disable CSRF for REST APIs
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
